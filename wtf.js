a5f555bdf109573650ee498321d4a8f71ce25cbc
JP Smith <jp.smith@ilstu.edu>
2015-01-25 10:39:44 -0600

    js ideas

---
+++ wtf.js
@@ -0,0 +1,6 @@
+if ( Math.max(-1, x) != Math.max(-1, x) && x == x    && !isFinite(x)            // x = undefined
+  && y == True                          && y == 1    && y !== 1                 // y = [1]
+  && 'hello'.charAt(z) != 'hello'[z]    && z != True && Math.max(z, null) === 0 // z = -1
+   ) {
+     console.log("flag")
+   }
