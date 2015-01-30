f3b7da8a108b9b1e3e639aacc789d76f1faf74f1
JP Smith <jp.smith@ilstu.edu>
2015-01-29 22:21:07 -0600

    magic

--- loljs/wtf.js
+++ loljs/wtf.js
@@ -1,6 +1,7 @@
-if ( Math.max(-1, x) != Math.max(-1, x) && x == x    && !isFinite(x)            // x = undefined
-  && y == true                          && y == 1    && y !== 1                 // y = [1]
+x = [1,2,3][x]
+if ( y == true                          && y == 1    && y !== 1                 // y = [1]
   && 'hello'.charAt(z) != 'hello'[z]    && z != true && Math.max(z, null) === 0 // z = -1
+  && Math.max(-1, x) != Math.max(-1, x) && x == x    && !isFinite(x)            // x = undefined
    ) {
      console.log("flag");
    }
