b61352910918017f9892941141d2943baf12eda7
JP Smith <jp.smith@ilstu.edu>
2015-01-31 14:34:07 -0600

    updated x to be correct

--- loljs/wtf.js
+++ loljs/wtf.js
@@ -2,7 +2,7 @@ x = [-Infinity,-1,0,1,Infinity][x];
 
 if ( y == true                          && y == 1    && y !== 1                 // y = [1]
   && 'hello'.charAt(z) != 'hello'[z]    && z != true && Math.max(z, null) === 0 // z = -1
-  && Math.max(-1, x) != Math.max(-1, x) && x == x    && !isFinite(x)            // x = undefined
+  && Math.max(-1, x) != Math.max(-1, x) && x == x    && !isFinite(x)            // x = 10
    ) {
      console.log("flag");
    }
